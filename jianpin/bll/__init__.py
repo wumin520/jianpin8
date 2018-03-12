dd512491ff0e0d1ec9b17a47fab266ef4dfd48b6
James <13764242761@139.com>
2018-03-12 15:12:26 +0800

    base bll

--- jianpin/bll/__init__.py
+++ jianpin/bll/__init__.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+import threading
+
+
+class BaseBll(object):
+    """
+    基础服务类，是单例。
+
+    类的生命周期为第一次获取实例初始化后永不销毁，除非显式移除
+
+    后面考虑根据Flask的请求周期进行销毁工作
+    """
+
+    instance = None
+
+    @classmethod
+    def get_instance(cls):
+        with threading.Lock():
+            if cls.instance is not None:
+                return cls.instance
+
+            cls.instance = cls()
+            return cls.instance
