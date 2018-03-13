becf9f090841aeb03d708e133988bf114eab7d04
James <13764242761@139.com>
2018-03-13 18:08:13 +0800

    db config

--- config.py
+++ config.py
@@ -2,9 +2,8 @@
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@fp02.ops.gaoshou.me:3307/zhuanqian?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@fp02.ops.gaoshou.me:3307/jianpin?charset=utf8'
 
 SQLALCHEMY_BINDS = {
-    'jianpin': 'mysql+pymysql://root@fp02.ops.gaoshou.me:3307/zq_admin?charset'
-            '=utf8'
+    'jianpin': 'mysql+pymysql://root@fp02.ops.gaoshou.me:3307/jianpin?charset=utf8'
 }
