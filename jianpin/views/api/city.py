e09c312fbee127def2f5aee33ce1fab5b41d2d52
James <13764242761@139.com>
2018-03-13 18:11:27 +0800

    city.list接口

--- jianpin/views/api/city.py
+++ jianpin/views/api/city.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+from jianpin import app
+from jianpin.bll.city import CityBll
+from jianpin.views.api import success
+
+
+@app.route('/api/city.list')
+def city_list():
+    """
+
+    :return:
+    """
+
+    cities = CityBll.get_instance().fetch_all()
+    payload = []
+    for city in cities:
+        payload.append({
+            'id': city.id,
+            'city_name': city.city_name
+        })
+
+    return success(payload)
