adace8aecf98016f6b6bc71431c1a32290c38682
James <13764242761@139.com>
2018-03-14 11:18:15 +0800

    error message

--- jianpin/views/api/__init__.py
+++ jianpin/views/api/__init__.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from flask import jsonify
 
 from jianpin import app
 
+logger = logging.getLogger(__name__)
+
 
 @app.errorhandler(Exception)
 def handle_api_exception(e):
+    logger.error(e)
     data = dict(err_code=1, err_msg=str(e))
     res = jsonify(data)
     res.status_code = 400
