0c15bfc32c9a1c7ae055ac6806b47d6c83a9d20b
James <13764242761@139.com>
2018-03-14 11:27:24 +0800

    api exception

--- jianpin/views/api/__init__.py
+++ jianpin/views/api/__init__.py
@@ -8,12 +8,47 @@ from jianpin import app
 logger = logging.getLogger(__name__)
 
 
-@app.errorhandler(Exception)
+class APIException(Exception):
+    """
+    API 异常
+    """
+
+    def __init__(self, status_code=400, err_code=0, err_msg='参数错误',
+                 payload=None, messages=[]):
+        """
+
+        :params status_code: HTTP Status Code
+        :parmas err_code: Error Code
+        :params err_msg: Error Message
+        :params payload: append data
+        :params messages: 消息
+        """
+
+        self.status_code = status_code
+        self.err_code = err_code
+        self.err_msg = err_msg
+        self.payload = payload
+        self.messages = messages
+
+    def __repr__(self):
+        return '%s %s %s' % (self.status_code, self.err_code, self.err_msg)
+
+    def to_dict(self):
+        rv = {
+            'err_code': self.err_code,
+            'err_msg': self.err_msg
+        }
+        if self.payload:
+            rv['payload'] = self.payload
+        if self.messages:
+            rv['messages'] = self.messages
+        return rv
+
+
+@app.errorhandler(APIException)
 def handle_api_exception(e):
-    logger.error(e)
-    data = dict(err_code=1, err_msg=str(e))
-    res = jsonify(data)
-    res.status_code = 400
+    res = jsonify(e.to_dict())
+    res.status_code = e.status_code
     return res
 
 
