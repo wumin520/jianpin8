3c19e02ac48a1fac0c5e6facb834584488b8b8eb
王艳 <wangyan@qianka.com>
2018-03-15 11:07:32 +0800

    优化代码

--- jianpin/views/api/detail.py
+++ jianpin/views/api/detail.py
@@ -5,7 +5,7 @@ from jianpin import app
 from jianpin.bll.city import CityBll
 from jianpin.bll.job import JobBll
 from jianpin.views.api import success, APIException, get_or_exception
-
+from jianpin.consts import JIANPIN_CURRENCY_UNIT, JIANPIN_JIEXI_TYPE
 
 @app.route('/api/job.detail')
 def city_list():
@@ -28,8 +28,8 @@ def city_list():
         'city_name': city.city_name,
         'hot_tag': job.hot_tag,
         'currency': job.currency,
-        'currency_unit': job.currency_unit,
-        'jiexi_type': job.jiexi_type,
+        'currency_unit': JIANPIN_CURRENCY_UNIT[job.currency_unit],
+        'jiexi_type': JIANPIN_JIEXI_TYPE[job.jiexi_type],
         'jianpin_person': job.jianpin_person,
         'jianpin_detail': job.jianpin_detail,
         'welfare': job.welfare,
